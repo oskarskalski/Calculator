83214ad89d54aa7524c9fd71f62a4b971334680e
Oskar <os.sk.gr@gmail.com>
2020-05-23 17:15:34 +0200

    Calculator version 0.0.4

---
+++ BtnSettings.java
@@ -0,0 +1,17 @@
+package sample;
+
+class BtnSettings {
+    public String[] getBtnNames(int index){
+        String[][] btnNames = {
+                {"MC", "M+", "M-", "MR", "C"},
+                {"1", "2", "3", "/", "<"},
+                {"4", "5", "6", "*", "%"},
+                {"7", "8", "9", "-", "x²"},
+                {".", "0", "=", "+", "/x"}
+        };
+
+        String[] getBtnRow = btnNames[index];
+
+        return getBtnRow;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private Scene scene;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        GUI gui = new GUI();
+
+        scene = new Scene(gui.getLayout(0), 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
