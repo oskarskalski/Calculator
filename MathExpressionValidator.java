3d90c7f46875afe2a53b046f689d5adf3fc89df1
Oskar <os.sk.gr@gmail.com>
2020-05-24 03:10:41 +0200

    Calculator version 0.1

--- MathExpressionValidator.java
+++ MathExpressionValidator.java
@@ -4,23 +4,27 @@ class MathExpressionValidator {
     public String validate(String expression, String character) {
         StringBuilder sb = new StringBuilder();
         int expressionMathLength = expression.length();
-
-        if(expression.equals("0") && isNumber(character)){
+        if (character.equals("<")) {
+            if (expression.length() == 1) {
+                sb.append(0);
+            } else {
+                sb.append(deleteElementsFromString(expression));
+            }
+        } else if (expression.equals("0") && isNumber(character)) {
             sb.append(character);
-        }else if (isNumber(character)) {
+        } else if (isNumber(character)) {
             if (!isNumber(String.valueOf(expression.charAt(expressionMathLength - 1)))) {
                 sb.append(expression);
                 sb.append(" ");
                 sb.append(character);
-            }else{
+            } else {
                 sb.append(expression);
                 sb.append(character);
             }
         } else if (expressionMathLength > 0) {
             if (!isNumber(String.valueOf(expression.charAt(expressionMathLength - 2)))) {
-                for (int i = 0; i < expressionMathLength - 2; i++) {
-                    sb.append(expression.charAt(i));
-                }
+                sb.append(deleteElementsFromString(expression));
+
                 sb.append(" ");
                 sb.append(character);
             } else {
@@ -28,6 +32,20 @@ class MathExpressionValidator {
                 sb.append(" ");
                 sb.append(character);
             }
+        }
+
+        return sb.toString();
+    }
+
+    private String deleteElementsFromString(String mathExpression) {
+        StringBuilder sb = new StringBuilder();
+        int mathExpressionLength = mathExpression.length() - 1;
+        if (!isNumber(String.valueOf(mathExpression.charAt(mathExpressionLength)))) {
+            mathExpressionLength = mathExpressionLength - 1;
+        }
+
+        for (int i = 0; i < mathExpressionLength; i++) {
+            sb.append(mathExpression.charAt(i));
         }
 
         return sb.toString();
